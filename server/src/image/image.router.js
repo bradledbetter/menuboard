f94f55276167e097cdbddea79f5e95f4c81249e5
Brad Ledbetter <bradley.ledbetter@e-hps.com>
2018-03-01 16:00:35 -0500

    Initial busboy test implementation in image upload. Just to see if it works

--- server/src/image/image.router.js
+++ server/src/image/image.router.js
@@ -1,5 +1,6 @@
 const restifyErrors = require('restify-errors');
 const ImageController = require('./image.controller');
+const Busboy = require('busboy');
 
 /**
  * Image Router - bind controller functions to routes
@@ -57,9 +58,39 @@ module.exports = (server) => {
 
     // TODO: upload a file and create an image record
     server.post('/image/upload', (req, res, next) => {
+        // another option that I don't like for this use case: https://leonid.shevtsov.me/post/demystifying-s3-browser-upload/
         // NOTE: req.body contains form fields, while req.file contains file data
-        // https://medium.com/technoetics/handling-file-upload-in-nodejs-7a4bb9f09a27
-        next(new restifyErrors.NotFoundError);
+        // https://github.com/mscdex/busboy
+        try {
+            const busboy = new Busboy({headers: req.headers});
+        } catch (ex) {
+            return next(new restifyErrors.InternalServerError(ex));
+        }
+
+        // handle file stream
+        busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+            console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+
+            file.on('data', function(data) {
+                console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+            });
+
+            file.on('end', function() {
+                console.log('File [' + fieldname + '] Finished');
+            });
+        });
+
+        // handle fields
+        busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+            console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+        });
+
+        busboy.on('finish', function() {
+            console.log('Done parsing form!');
+            res.send(200, {Connection: 'close'});
+            next();
+        });
+
     });
 
     // udpate an image
