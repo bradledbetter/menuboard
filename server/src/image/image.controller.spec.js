250141d504233b929eb9568e8bd07079a886c5ed
Brad Ledbetter <bradley.ledbetter@e-hps.com>
2018-03-16 15:45:21 -0400

    fixing broken unit tests

--- server/src/image/image.controller.spec.js
+++ server/src/image/image.controller.spec.js
@@ -1,6 +1,12 @@
+// NOTE: make sure to proxyquire before requiring the files that require the proxied things
+const proxyquire = require('proxyquire');
+const mockLogger = require('../services/logger.stub');
+proxyquire('./image.controller', {'../services/logger.service': mockLogger});
+
 const ImageModel = require('./image.model');
 const ImageController = require('./image.controller');
 const restifyErrors = require('restify-errors');
+const Promise = require('bluebird');
 
 describe('ImageController', () => {
     let controller;
@@ -9,23 +15,16 @@ describe('ImageController', () => {
     const image = {
         _id: '1',
         label: originalLabel,
-        url: originalURL,
-        save: jasmine.createSpy('image.save').and.returnValue({
-            then: (success) => {
-                success('bla');
-                return {
-                    catch: () => {}
-                };
-            }
-        }),
-        delete: jasmine.createSpy('image.delete').and.returnValue({
-            then: (success) => {
-                success('bla');
-                return {
-                    catch: () => {}
-                };
-            }
-        })
+        url: originalURL
+    };
+    image.save = jasmine.createSpy('image.save').and.returnValue(Promise.resolve('bla'));
+    image.delete = jasmine.createSpy('image.delete').and.returnValue(Promise.resolve('bla'));
+
+    const fakeQuery = {
+        select: () => {},
+        exec: () => {
+            return Promise.resolve({});
+        }
     };
 
     beforeEach(() => {
@@ -33,8 +32,8 @@ describe('ImageController', () => {
     });
 
     it('should be able to find one or many images', () => {
-        spyOn(ImageModel, 'find').and.callThrough();
-        spyOn(ImageModel, 'findOne').and.callThrough();
+        spyOn(ImageModel, 'find').and.returnValue(fakeQuery);
+        spyOn(ImageModel, 'findOne').and.returnValue(fakeQuery);
 
         let promise = controller.findImages('1', ['label']);
         expect(promise).toEqual(jasmine.any(Promise));
@@ -46,163 +45,99 @@ describe('ImageController', () => {
     });
 
     describe('createImage', () => {
-        const myResolve = jasmine.createSpy('myResolve');
-        const myReject = jasmine.createSpy('myReject');
-
         beforeEach(() => {
-            spyOn(global, 'Promise').and.callFake((callback) => {
-                callback(myResolve, myReject);
-            });
-
-            spyOn(ImageModel, 'create').and.callFake((params) => {
-                return {
-                    exec: () => {
-                        return {
-                            then: (callback) => {
-                                callback(image);
-                                return {catch: () => {}};
-                            }
-                        };
-                    }
-                };
-            });
+            spyOn(ImageModel, 'create').and.returnValue(Promise.resolve(image));
         });
 
-        afterEach(() => {
-            myResolve.calls.reset();
-            myReject.calls.reset();
-        });
-
-        it('should be able to create a new image with valid fields', () => {
-            controller.createImage(image);
-            expect(ImageModel.create).toHaveBeenCalled();
-            expect(myResolve).toHaveBeenCalled();
-            expect(myReject).not.toHaveBeenCalled();
+        it('should be able to create a new image with valid fields', (done) => {
+            controller.createImage(image)
+                .then(() => {
+                    expect(ImageModel.create).toHaveBeenCalled();
+                    expect(mockLogger.info).toHaveBeenCalled();
+                    done();
+                });
         });
 
         // NOTE: image relies on mongo for model validation, so testing the controller doesn't make sense
     });
 
     describe('updateImage', () => {
-        const myResolve = jasmine.createSpy('myResolve');
-        const myReject = jasmine.createSpy('myReject');
         const newImage = {
             label: 'New iamge label',
             url: 'http://newimage.com/newimage.jpg'
         };
 
-        beforeEach(() => {
-            spyOn(global, 'Promise').and.callFake((callback) => {
-                callback(myResolve, myReject);
-            });
-        });
-
         afterEach(() => {
-            myResolve.calls.reset();
-            myReject.calls.reset();
             image.save.calls.reset();
             image.label = originalLabel;
             image.url = originalURL;
+            mockLogger.info.calls.reset();
         });
 
-        it('should save an updated image with valid fields', () => {
-            spyOn(ImageModel, 'findOne').and.returnValue({
-                exec: () => {
-                    return {
-                        then: (callback) => {
-                            callback(image);
-                            return {
-                                catch: () => {}
-                            };
-                        }
-                    };
-                }
-            });
+        it('should save an updated image with valid fields', (done) => {
+            spyOn(ImageModel, 'findOne').and.returnValue(Promise.resolve(image));
 
-            controller.updateImage('1', newImage);
-            expect(myResolve).toHaveBeenCalled();
-            expect(myReject).not.toHaveBeenCalled();
-            expect(image.save).toHaveBeenCalled();
-            expect(image.label).toEqual(newImage.label);
-            expect(image.url).toEqual(newImage.url);
+            controller.updateImage('1', newImage)
+                .then(() => {
+                    expect(mockLogger.info).toHaveBeenCalled();
+                    expect(image.save).toHaveBeenCalled();
+                    expect(image.label).toEqual(newImage.label);
+                    expect(image.url).toEqual(newImage.url);
+                    done();
+                });
         });
 
-        it('should not override an image when invalid data is PUT', () => {
-            spyOn(ImageModel, 'findOne').and.returnValue({
-                exec: () => {
-                    return {
-                        then: (callback) => {
-                            callback(image);
-                            return {
-                                catch: () => {}
-                            };
-                        }
-                    };
-                }
-            });
+        it('should not override an image when invalid data is PUT', (done) => {
+            spyOn(ImageModel, 'findOne').and.returnValue(Promise.resolve(image));
 
-            controller.updateImage('1', {label: '', url: ''});
-            expect(myResolve).toHaveBeenCalled();
-            expect(myReject).not.toHaveBeenCalled();
-            myReject.calls.reset();
-            expect(image.save).toHaveBeenCalled();
-            expect(image.label).not.toEqual('');
-            expect(image.url).not.toEqual('');
+            controller.updateImage('1', {label: '', url: ''})
+                .then(() => {
+                    expect(image.save).toHaveBeenCalled();
+                    expect(image.label).not.toEqual('');
+                    expect(image.url).not.toEqual('');
+                    done();
+                });
         });
 
-        it('should not save an image that does not exist', () => {
-            controller.updateImage('', newImage);
-            expect(myResolve).not.toHaveBeenCalled();
-            expect(myReject).toHaveBeenCalled();
-            expect(image.save).not.toHaveBeenCalled();
-            expect(image.label).not.toEqual(newImage.label);
+        it('should not save an image that does not exist', (done) => {
+            controller.updateImage('', newImage)
+                .catch((error) => {
+                    expect(image.save).not.toHaveBeenCalled();
+                    expect(image.label).not.toEqual(newImage.label);
+                    expect(error).toEqual(jasmine.any(restifyErrors.ForbiddenError));
+                    done();
+                });
         });
     });
 
     describe('deleteImage', () => {
-        const myResolve = jasmine.createSpy('myResolve');
-        const myReject = jasmine.createSpy('myReject');
-        beforeEach(() => {
-            spyOn(global, 'Promise').and.callFake((callback) => {
-                callback(myResolve, myReject);
-            });
-        });
-
         afterEach(() => {
-            myResolve.calls.reset();
-            myReject.calls.reset();
             image.delete.calls.reset();
             images = [];
+            mockLogger.info.calls.reset();
         });
 
-        it('should delete image', () => {
-            spyOn(ImageModel, 'findOne').and.returnValue({
-                exec: () => {
-                    return {
-                        then: (callback) => {
-                            callback(image);
-                            return {
-                                catch: () => {}
-                            };
-                        }
-                    };
-                }
-            });
+        it('should delete image', (done) => {
+            spyOn(ImageModel, 'findOne').and.returnValue(Promise.resolve(image));
 
-            controller.deleteImage(image._id);
-            expect(ImageModel.findOne).toHaveBeenCalled();
-            expect(image.delete).toHaveBeenCalled();
-            expect(myResolve).toHaveBeenCalledWith('Success');
-            expect(myReject).not.toHaveBeenCalled();
+            controller.deleteImage(image._id)
+                .then(() => {
+                    expect(ImageModel.findOne).toHaveBeenCalled();
+                    expect(image.delete).toHaveBeenCalled();
+                    expect(mockLogger.info).toHaveBeenCalled();
+                    done();
+                });
         });
 
-        it('should not delete an image if no id provided', () => {
+        it('should not delete an image if no id provided', (done) => {
             spyOn(ImageModel, 'findOne');
-            controller.deleteImage();
-            expect(ImageModel.findOne).not.toHaveBeenCalled();
-            expect(image.delete).not.toHaveBeenCalled();
-            expect(myResolve).not.toHaveBeenCalledWith();
-            expect(myReject).toHaveBeenCalledWith(jasmine.any(restifyErrors.ForbiddenError));
+            controller.deleteImage()
+                .catch((error) => {
+                    expect(ImageModel.findOne).not.toHaveBeenCalled();
+                    expect(image.delete).not.toHaveBeenCalled();
+                    expect(error).toEqual(jasmine.any(restifyErrors.ForbiddenError));
+                    done();
+                });
         });
     });
 });
