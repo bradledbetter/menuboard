0249e1729282bccceed84b1b93a06417a86c4168
Brad Ledbetter <bradley.ledbetter@e-hps.com>
2018-03-28 13:33:25 -0400

    Adding unit tests for upload service

--- server/src/upload/upload.service.js
+++ server/src/upload/upload.service.js
@@ -5,53 +5,50 @@ const aws = require('aws-sdk');
 const Promise = require('bluebird');
 
 /**
- * A class that will house various methods useful in handling file uploads
+ * Return an upload URL and AWS S3 fields for the client to upload direct to S3
+ * @param {string} filename name of the
+ * @return {Promise} resolved with credentials object or rejected with an error
+ * @throws restify error if the file extension isn't one that's allowed
  */
-class UploadService {
-    /**
-     * Return an upload URL and AWS S3 fields for the client to upload direct to S3
-     * @param {string} filename name of the
-     * @return {Promise} resolved with credentials object or rejected with an error
-     * @throws restify error if the file extension isn't one that's allowed
-     */
-    static s3Credentials(filename) {
-        // file extension check
-        if (environment.aws.s3.allowedExtensions) {
-            const ext = filename.substr(filename.lastIndexOf('.'));
-            if (!environment.aws.s3.allowedExtensions.has(ext)) {
-                return Promise.reject(new restifyErrors.NotAcceptableError());
-            }
+function s3Credentials(filename) {
+    // file extension check
+    if (environment.aws.s3.allowedExtensions) {
+        const ext = filename.substr(filename.lastIndexOf('.') + 1);
+        if (!environment.aws.s3.allowedExtensions.has(ext)) {
+            return Promise.reject(new restifyErrors.NotAcceptableError());
         }
-
-        const s3 = new aws.S3({
-            signatureVersion: environment.aws.s3.signatureVersion,
-            region: environment.aws.region,
-            accessKeyId: environment.aws.credentials.accessKeyId,
-            secretAccessKey: environment.aws.credentials.secretAccessKey
-        });
-
-        const params = {
-            Bucket: environment.aws.s3.bucket,
-            Fields: {
-                key: filename,
-            },
-            Expires: environment.aws.s3.signatureExpiration,
-            Conditions: [
-                ['content-length-range', 0, environment.aws.s3.maxFileSizeBytes],
-                {acl: 'public-read'},
-                {success_action_status: '201'},
-            ],
-        };
-
-        const s3CreatePresignedPost = Promise.promisify(s3.createPresignedPost, {context: s3});
-        return s3CreatePresignedPost(params)
-            .then((data) => {
-                const transformedData = Object.assign({}, data);
-                transformedData.fields.acl = 'public-read';
-                transformedData.fields.success_action_status = '201';
-                return transformedData;
-            });
     }
+
+    const s3 = new aws.S3({
+        signatureVersion: environment.aws.s3.signatureVersion,
+        region: environment.aws.region,
+        accessKeyId: environment.aws.credentials.accessKeyId,
+        secretAccessKey: environment.aws.credentials.secretAccessKey
+    });
+
+    const params = {
+        Bucket: environment.aws.s3.bucket,
+        Fields: {
+            key: filename,
+        },
+        Expires: environment.aws.s3.signatureExpiration,
+        Conditions: [
+            ['content-length-range', 0, environment.aws.s3.maxFileSizeBytes],
+            {acl: 'public-read'},
+            {success_action_status: '201'},
+        ],
+    };
+
+    const s3CreatePresignedPost = Promise.promisify(s3.createPresignedPost, {context: s3});
+    return s3CreatePresignedPost(params)
+        .then((data) => {
+            const transformedData = Object.assign({}, data);
+            transformedData.fields.acl = 'public-read';
+            transformedData.fields.success_action_status = '201';
+            return transformedData;
+        });
 }
 
-module.exports = UploadService;
+module.exports = {
+    s3Credentials
+};
