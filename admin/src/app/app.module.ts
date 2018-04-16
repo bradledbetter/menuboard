b7e4dbb5da8c12b544868068a4045b8b99ec3689
Brad Ledbetter <bradley.ledbetter@e-hps.com>
2018-04-16 13:56:35 -0400

    Adding Angular Material

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { UploaderComponent } from './uploader/uploader.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -13,7 +17,10 @@ import { UploaderComponent } from './uploader/uploader.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
