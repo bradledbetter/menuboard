2953358c1d94d3a041e28bc6191e7ad53d988d83
Brad Ledbetter <bradley.ledbetter@e-hps.com>
2018-04-27 08:26:55 -0400

    Beginning to integrate login, auth, app

--- admin/src/app/auth/auth.service.ts
+++ admin/src/app/auth/auth.service.ts
@@ -1,15 +1,56 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import { ConfigService } from './../config/config.service';
 @Injectable()
 export class AuthService {
+  private baseHeaders: HttpHeaders;
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private config: ConfigService) {
+    this.baseHeaders = new HttpHeaders({
+      Accept: 'application/json',
+      'Content-type': 'application/json'
+    });
+  }
+
+  /**
+   * Call to login
+   * @param {string} username the username to login with
+   * @param {string} password the password to login with
+   * @returns {Observable}
+   * @throws on API error, for now
+   */
+  login(username: string, password: string): Observable<any> {
+    // TODO: check that this works, clean up bugs, figure out what I want to do with API errors
+    return this.http.post(this.config.get('api') + '/login', { username, password }, { headers: this.baseHeaders })
+      .catch((error) => {
+        console.error(error);
+        throw new Error(error);
+      });
+  }
+
+  /**
+   * Logout a user
+   * @returns {Observable}
+   * @throws on API error, for now
+   */
+  logout(): Observable<any> {
+    // TODO: check that this works, clean up bugs, figure out what I want to do with API errors
+    return this.http.post(this.config.get('api') + '/logout', {}, { headers: this.baseHeaders })
+      .catch((error) => {
+        console.error(error);
+        throw new Error(error);
+      });
+  }
 
   /**
    * Check whether or not someone is logged in or not
    * @returns {boolean} true if someone is authenticated, false otherwise
    */
   isAuthenticated() {
+    // TODO: check with session
     return false;
   }
 }
--- admin/src/app/login/login.component.ts
+++ admin/src/app/login/login.component.ts
@@ -1,18 +1,20 @@
+import { AuthService } from './../auth/auth.service';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'mba-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-// https://github.com/cornflourblue/angular2-registration-login-example-cli
+  // https://github.com/cornflourblue/angular2-registration-login-example-cli
   public passwordMinLength = 12;
   public email: FormControl;
   public password: FormControl;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private auth: AuthService) { }
 
   ngOnInit() {
     this.email = this.formBuilder.control('', [Validators.required, Validators.email]);
@@ -25,6 +27,12 @@ export class LoginComponent implements OnInit {
    */
   login() {
     // TODO:
+    this.auth
+      .login(this.email.value, this.password.value)
+      .subscribe((retVal) => {
+        // TODO: set session
+        console.log('logged in');
+      });
   }
 
 }
